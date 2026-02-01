ac4d12333e12529acf5bff613f820ff92c5407c0
Imaginary Gifts <imaginarygifts.in@gmail.com>
2026-02-01 17:50:11 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDaeaJy8haKhn3Ve5rUdrj7XItXPI-ujDU",
-  authDomain: "sellfix-designing.firebaseapp.com",
-  projectId: "sellfix-designing",
-  storageBucket: "sellfix-designing.firebasestorage.app",
-  messagingSenderId: "129826052151",
-  appId: "1:129826052151:web:ff6f1cb5fce219d65087b2"
-};
-
+    apiKey: "AIzaSyB8_TFSHHIk3HHcDC4478R1jBQCS6nvsnQ",
+    authDomain: "login-88aab.firebaseapp.com",
+    projectId: "login-88aab",
+    storageBucket: "login-88aab.firebasestorage.app",
+    messagingSenderId: "460715420280",
+    appId: "1:460715420280:web:54d129a73398631dbfdb8d",
+    measurementId: "G-G6JL99V8C3"
+  };
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
